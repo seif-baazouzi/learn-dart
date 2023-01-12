776d96834cd0ffd66cff07b319198ec6ccd27197
seif <baazouziseif9@gmail.com>
2023-01-12 12:23:27 +0100

    variables

---
+++ src/02-variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "seif";
+  int age = 22;
+  bool isMale = true;
+  double money = 20.25;
+
+  print("name   = $name");
+  print("age    = $age ");
+  print("gender = ${isMale ? "male" : "female"}");
+  print("money  = $money");
+}
