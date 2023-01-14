f56ed33fe84e07ec862057f387cb731310e92ed1
seif <baazouziseif9@gmail.com>
2023-01-14 08:23:10 +0100

    assign varibales

---
+++ src/06-assign-varibales.dart
@@ -0,0 +1,20 @@
+void main() {
+  int a = 20;
+
+  a += 5;
+  print(a);
+
+  a -= 5;
+  print(a);
+
+  a *= 5;
+  print(a);
+
+  a ~/= 5;
+  print(a);
+
+  int? x = null;
+  x ??= 50;
+
+  print(x);
+}
