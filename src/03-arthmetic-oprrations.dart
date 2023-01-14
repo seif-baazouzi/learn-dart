2dabc00b374049ebc591b180560fdb88a8c52bba
seif <baazouziseif9@gmail.com>
2023-01-14 08:09:33 +0100

    arthmetic oprrations

---
+++ src/03-arthmetic-oprrations.dart
@@ -0,0 +1,14 @@
+void main() {
+  int a = 20;
+  int b = 10;
+
+  a++; // a = a + 1
+  a--; // a = a - 1
+
+  print(a + b); // add
+  print(a - b); // sub
+  print(a * b); // mul
+  print(a / b); // div
+  print(a % b); // mod
+  print(a ~/ b); // integer div
+}
