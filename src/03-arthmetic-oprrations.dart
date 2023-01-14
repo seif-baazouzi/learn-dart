6da441f74ed3f329fe86210995d9ad69d3da826b
seif <baazouziseif9@gmail.com>
2023-01-14 08:13:53 +0100

    logical operations

--- src/03-arthmetic-oprrations.dart
+++ src/03-arthmetic-oprrations.dart
@@ -2,13 +2,13 @@ void main() {
   int a = 20;
   int b = 10;
 
-  a++; // a = a + 1
-  a--; // a = a - 1
+  a++;
+  a--;
 
-  print(a + b); // add
-  print(a - b); // sub
-  print(a * b); // mul
-  print(a / b); // div
-  print(a % b); // mod
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
   print(a ~/ b); // integer div
 }
