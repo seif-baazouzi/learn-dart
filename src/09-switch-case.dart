b8533a567a3d838268fc6c2c0c525c348d3099bb
seif <baazouziseif9@gmail.com>
2023-01-15 07:39:33 +0100

    switch case

---
+++ src/09-switch-case.dart
@@ -0,0 +1,20 @@
+void main() {
+  int money = 100;
+
+  switch (money) {
+    case 10:
+      {
+        print("Money = 10");
+        break;
+      }
+    case 50:
+      {
+        print("Money = 50");
+        break;
+      }
+    default:
+      {
+        print("Money = $money");
+      }
+  }
+}
