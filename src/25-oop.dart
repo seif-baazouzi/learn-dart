c2ec840cf05bb3c84e60460d192eb7dec5c8ab4f
seif <baazouziseif9@gmail.com>
2023-01-18 13:52:57 +0100

    classes setters and getters

--- src/25-oop.dart
+++ src/25-oop.dart
@@ -1,15 +1,33 @@
 void main() {
   var phone = new Phone("phone", 5.6);
+  phone.name = "new phone name";
+
   print(phone);
 }
 
 class Phone {
-  String name = "";
-  double screen = 0.0;
+  String _name = "";
+  double _screen = 0.0;
 
-  Phone(this.name, this.screen) {}
+  Phone(this._name, this._screen) {}
+
+  String get name {
+    return _name;
+  }
+
+  double get screen {
+    return this._screen;
+  }
+
+  void set name(String newName) {
+    this._name = newName;
+  }
+
+  void set screen(double newScreen) {
+    this._screen = newScreen;
+  }
 
   String toString() {
-    return "${this.name}, ${this.screen}";
+    return "${this._name}, ${this._screen}";
   }
 }
